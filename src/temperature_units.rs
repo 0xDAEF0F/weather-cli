c88650ff6c453ecd105b9068e6ca62f22c387063
daemon <0xDAEF0F@proton.me>
2024-02-12 18:01:11 -0700

    checkpoint: about to add http calls to request for weather 2

---
+++ src/temperature_units.rs
@@ -0,0 +1,30 @@
+#[derive(Clone)]
+pub enum TempUnits {
+    Fahrenheit,
+    Celsius,
+    Kelvin,
+}
+
+impl std::fmt::Display for TempUnits {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            TempUnits::Celsius => "celsius",
+            TempUnits::Fahrenheit => "fahrenheit",
+            TempUnits::Kelvin => "kelvin",
+        };
+        s.fmt(f)
+    }
+}
+
+impl std::str::FromStr for TempUnits {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "fahrenheit" => Ok(Self::Fahrenheit),
+            "celsius" => Ok(Self::Celsius),
+            "kelvin" => Ok(Self::Kelvin),
+            _ => Err(format!("Unknown units: {s}")),
+        }
+    }
+}
